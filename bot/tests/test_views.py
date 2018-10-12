7bd5c5f7b32153c38df77b3d9f573bd62858ae51
SebastienLesouef <sebastienlesouef@gmail.com>
2018-10-12 13:55:18 +0200

    added test for loading index.html

--- bot/tests/test_views.py
+++ bot/tests/test_views.py
@@ -1,6 +1,11 @@
 from bot.views import app
 
-def test_route():
+def test_route_default():
     client = app.test_client()
     response = client.get('/')
+    assert response.status_code == 200
+
+def test_route_index():
+    client = app.test_client()
+    response = client.get('/index')
     assert response.status_code == 200
