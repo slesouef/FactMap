7f9b6f9dbb69e730e1eed9db0e8e2b9dd7d7538d
SebastienLesouef <sebastienlesouef@gmail.com>
2018-10-13 20:35:01 +0200

    added tests for header and title

--- bot/tests/test_views.py
+++ bot/tests/test_views.py
@@ -1,11 +1,27 @@
 from bot.views import app
 
+
 def test_route_default():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
 
+
 def test_route_index():
     client = app.test_client()
-    response = client.get('/index')
+    response = client.get('/index/')
     assert response.status_code == 200
+
+
+def test_header():
+    client = app.test_client()
+    response = client.get('/index/')
+    rv = response.get_data(as_text=True)
+    assert '<head>' in rv
+
+
+def test_title():
+    client = app.test_client()
+    response = client.get('/')
+    rv = response.get_data(as_text=True)
+    assert 'FactMaps - infobot' in rv
