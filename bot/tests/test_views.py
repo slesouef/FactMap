645df7eb4dfb97b896e41d75d0ce424434e22ee2
SebastienLesouef <sebastienlesouef@gmail.com>
2018-10-16 15:15:11 +0200

    following coverage check, refactored test_views and removed all html tests

--- bot/tests/test_views.py
+++ bot/tests/test_views.py
@@ -1,27 +1,16 @@
 from bot.views import app
 
 
-def test_route_default():
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
+class TestView:
 
+    @classmethod
+    def setup_class(cls):
+        cls.client = app.test_client()
 
-def test_route_index():
-    client = app.test_client()
-    response = client.get('/index/')
-    assert response.status_code == 200
+    def test_route_default(self):
+        response = self.client.get('/')
+        assert response.status_code == 200
 
-
-def test_title():
-    client = app.test_client()
-    response = client.get('/')
-    rv = response.get_data(as_text=True)
-    assert 'FactMaps - infobot' in rv
-
-
-def test_header():
-    client = app.test_client()
-    response = client.get('/index/')
-    rv = response.get_data(as_text=True)
-    assert '<header>' in rv
+    def test_route_index(self):
+        response = self.client.get('/index/')
+        assert response.status_code == 200
