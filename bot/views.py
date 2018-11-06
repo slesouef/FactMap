43920bb3438daf8c3d7eb640166cd21cdaa8b5bd
SebastienLesouef <sebastienlesouef@gmail.com>
2018-11-06 18:28:58 +0100

    added '/time' endpoint to views for basic ajax implementation

--- bot/views.py
+++ bot/views.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from datetime import datetime
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/index/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/time')
+def time():
+    return jsonify(datetime.now())
