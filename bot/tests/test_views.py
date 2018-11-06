41cf93ae6e5219cff89c7d8123c987a95781555a
SebastienLesouef <sebastienlesouef@gmail.com>
2018-11-06 18:28:19 +0100

    added test for /time endpoint for basic ajax implementation

--- bot/tests/test_views.py
+++ bot/tests/test_views.py
@@ -1,4 +1,5 @@
 from bot.views import app
+from datetime import datetime
 
 
 class TestView:
@@ -13,4 +14,8 @@ class TestView:
 
     def test_route_index(self):
         response = self.client.get('/index/')
+        assert response.status_code == 200
+
+    def test_route_time(self):
+        response = self.client.get('/time')
         assert response.status_code == 200
