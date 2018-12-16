95f7450189e2d0118deb033aa685ef3467c7d369
SebastienLesouef <sebastienlesouef@gmail.com>
2018-12-16 21:36:59 +0100

    modified js script to no longer accept empty values and to work with new css schema

--- bot/static/js/script.js
+++ bot/static/js/script.js
@@ -1,7 +1,12 @@
-document.getElementById('confirm').addEventListener("click", function(){
-    var entry = document.getElementById("question").value;
-    document.getElementById("question").value = "";
-    var new_entry = document.createElement("div");
-    new_entry.textContent = entry;
-    document.getElementById("messages").appendChild(new_entry);
+document.getElementById("submit").addEventListener("click", function(){
+    var entry = document.getElementById("input").value;
+    if (entry === '') {
+        return false;
+    } else {
+        document.getElementById("input").value = "";
+        var new_entry = document.createElement("div");
+        new_entry.textContent = entry;
+        new_entry.className = "message message-perso";
+        document.getElementById("messages").appendChild(new_entry);
+    }
 });
