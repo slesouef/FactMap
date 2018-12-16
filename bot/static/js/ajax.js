9175582cd97b7ed52e396f7a1e7115b93f8262b8
SebastienLesouef <sebastienlesouef@gmail.com>
2018-12-16 21:41:58 +0100

    modified ajax script to work with new css

--- bot/static/js/ajax.js
+++ bot/static/js/ajax.js
@@ -4,7 +4,7 @@ document.getElementById("button").addEventListener("click", function(e){
     req.open("GET", "http://localhost:5000/time");
     req.addEventListener("load", function(){
         if(req.status >= 200 && req.status < 400){
-            document.getElementById("messages").innerHTML = "<p>" + "<b>" +
+            document.getElementById("button").innerHTML = "<p>" + "<b>" +
                 req.responseText + "</b>" + "</p>";
         } else {
             console.error(req.status + " " + req.statusText + " " + url);
