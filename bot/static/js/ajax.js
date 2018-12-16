908bf7d3749706933c8d24d0fdb92a5e907544cc
SebastienLesouef <sebastienlesouef@gmail.com>
2018-12-16 22:11:53 +0100

    created getdata ajax function to retrieve info from server

--- bot/static/js/ajax.js
+++ bot/static/js/ajax.js
@@ -1,11 +1,12 @@
-document.getElementById("button").addEventListener("click", function(e){
-    e.preventDefault();
+function getdata(){
     var req = new XMLHttpRequest();
-    req.open("GET", "http://localhost:5000/time");
+    req.open("GET", "http://localhost:5000/data");
     req.addEventListener("load", function(){
         if(req.status >= 200 && req.status < 400){
-            document.getElementById("button").innerHTML = "<p>" + "<b>" +
-                req.responseText + "</b>" + "</p>";
+            var new_response = document.createElement("div");
+            new_response.textContent = req.responseText;
+            new_response.className = "message";
+            document.getElementById("messages").appendChild(new_response);
         } else {
             console.error(req.status + " " + req.statusText + " " + url);
         }
@@ -14,4 +15,4 @@ document.getElementById("button").addEventListener("click", function(e){
         console.error("Network error on " + url);
     });
     req.send(null);
-});
+}
