0909c6da8d3800a58210f1b0be757aa1e2d70ec5
SebastienLesouef <sebastienlesouef@gmail.com>
2018-12-20 19:15:21 +0100

    modified to provide POST body to ajax function

--- bot/static/js/script.js
+++ bot/static/js/script.js
@@ -1,13 +1,13 @@
-document.getElementById("submit").addEventListener("click", function(){
+document.getElementById("submit").addEventListener("click", function () {
     var entry = document.getElementById("input").value;
     if (entry === '') {
         return false;
     } else {
         document.getElementById("input").value = "";
-        var new_entry = document.createElement("div");
-        new_entry.textContent = entry;
-        new_entry.className = "message message-perso";
-        document.getElementById("messages").appendChild(new_entry);
-        getdata();
+        var newEntry = document.createElement("div");
+        newEntry.textContent = entry;
+        newEntry.className = "message message-personal";
+        document.getElementById("messages").appendChild(newEntry);
+        getData(entry);
     }
 });
