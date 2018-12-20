ca49e63a05c147fcf422f2005173be294d81747e
SebastienLesouef <sebastienlesouef@gmail.com>
2018-12-20 19:14:03 +0100

    modified request to use POST method

--- bot/static/js/ajax.js
+++ bot/static/js/ajax.js
@@ -1,18 +1,18 @@
-function getdata(){
+function getData(entry) {
     var req = new XMLHttpRequest();
-    req.open("GET", "http://localhost:5000/data");
-    req.addEventListener("load", function(){
-        if(req.status >= 200 && req.status < 400){
-            var new_response = document.createElement("div");
-            new_response.textContent = req.responseText;
-            new_response.className = "message";
-            document.getElementById("messages").appendChild(new_response);
+    req.open("POST", "http://localhost:5000/data");
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            var newResponse = document.createElement("div");
+            newResponse.textContent = req.responseText;
+            newResponse.className = "message";
+            document.getElementById("messages").appendChild(newResponse);
         } else {
             console.error(req.status + " " + req.statusText + " " + url);
         }
     });
-    req.addEventListener("error", function(){
+    req.addEventListener("error", function () {
         console.error("Network error on " + url);
     });
-    req.send(null);
+    req.send(entry);
 }
