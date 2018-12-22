fde1dc8d45944c6721b57a4d64368cbeddb020bb
SebastienLesouef <sebastienlesouef@gmail.com>
2018-12-22 10:58:41 +0100

    generalised getData to use callback

--- bot/static/js/ajax.js
+++ bot/static/js/ajax.js
@@ -1,13 +1,9 @@
-function getData(entry) {
+function getData(entry, callback) {
     var req = new XMLHttpRequest();
     req.open("POST", "http://localhost:5000/data");
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
-            var newResponse = document.createElement("div");
-            newResponse.textContent = req.responseText;
-            newResponse.className = "message";
-            document.getElementById("messages").appendChild(newResponse);
-            newResponse.scrollIntoView(false);
+            callback(req.responseText)
         } else {
             console.error(req.status + " " + req.statusText + " " + url);
         }
