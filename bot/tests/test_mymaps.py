d5aaac2a7bcf8a082e93d818b9283120dd8cc4c5
SebastienLesouef <sebastienlesouef@gmail.com>
2018-12-27 09:13:09 +0100

    added test module w/ TODO

---
+++ bot/tests/test_mymaps.py
@@ -0,0 +1,8 @@
+import bot.mymaps as script
+
+# TODO: create stub
+
+# TODO: create test class
+# TODO: test call to gmaps (from parser contnent)
+# TODO: test response from gmaps stub
+# TODO: test ajax response
