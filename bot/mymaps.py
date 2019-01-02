df5ff1bcb62caebd7e58d951f5882fccf85221cf
SebastienLesouef <sebastienlesouef@gmail.com>
2019-01-02 16:21:52 +0100

    geocode request mockup implemented. test green

---
+++ bot/mymaps.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python3
+# -*- coding:utf-8 -*-
+import requests
+
+
+def get_geocode():
+    url = 'test'
+    r = requests.get(url)
+    return r
--- bot/tests/test_mymaps.py
+++ bot/tests/test_mymaps.py
@@ -1,73 +1,29 @@
 import bot.mymaps as script
 
-geocode_reponse = {
-   "results" : [
-      {
-         "address_components" : [
-            {
-               "long_name" : "7",
-               "short_name" : "7",
-               "types" : [ "street_number" ]
-            },
-            {
-               "long_name" : "Cité Paradis",
-               "short_name" : "Cité Paradis",
-               "types" : [ "route" ]
-            },
-            {
-               "long_name" : "Paris",
-               "short_name" : "Paris",
-               "types" : [ "locality", "political" ]
-            },
-            {
-               "long_name" : "Paris",
-               "short_name" : "Paris",
-               "types" : [ "administrative_area_level_2", "political" ]
-            },
-            {
-               "long_name" : "Île-de-France",
-               "short_name" : "Île-de-France",
-               "types" : [ "administrative_area_level_1", "political" ]
-            },
-            {
-               "long_name" : "France",
-               "short_name" : "FR",
-               "types" : [ "country", "political" ]
-            },
-            {
-               "long_name" : "75010",
-               "short_name" : "75010",
-               "types" : [ "postal_code" ]
-            }
-         ],
-         "formatted_address" : "7 Cité Paradis, 75010 Paris, France",
-         "geometry" : {
-            "location" : {
-               "lat" : 48.8747578,
-               "lng" : 2.350564700000001
-            },
-            "location_type" : "ROOFTOP",
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 48.87610678029149,
-                  "lng" : 2.351913680291502
-               },
-               "southwest" : {
-                  "lat" : 48.87340881970849,
-                  "lng" : 2.349215719708499
-               }
-            }
-         },
-         "place_id" : "ChIJIZX8lhRu5kcRGwYk8Ce3Vc8",
-         "plus_code" : {
-            "compound_code" : "V9F2+W6 Paris, France",
-            "global_code" : "8FW4V9F2+W6"
-         },
-         "types" : [ "establishment", "point_of_interest" ]
-      }
-   ],
-   "status" : "OK"
-}
+import requests
+
+
+def test_geocode_request(monkeypatch):
+    geocode_response = {
+       "results" : [
+          {
+             "formatted_address" : "7 Cité Paradis, 75010 Paris, France",
+             "geometry" : {
+                "location" : {
+                   "lat" : 48.8747578,
+                   "lng" : 2.350564700000001
+                },
+             },
+          }
+       ],
+       "status" : "OK"
+    }
+
+    def mockreturn(request):
+        return geocode_response
+
+    monkeypatch.setattr(requests, 'get', mockreturn)
+    assert script.get_geocode() == geocode_response
 
 # TODO: create test class
 # TODO: test call to gmaps (from parser content)
