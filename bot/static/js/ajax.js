74643d011b2f17bce30af08b1246b1790fb78b99
SebastienLesouef <sebastienlesouef@gmail.com>
2019-01-03 11:13:24 +0100

    getData ajax call now uses relative path

--- bot/static/js/ajax.js
+++ bot/static/js/ajax.js
@@ -1,15 +1,15 @@
 function getData(entry, callback) {
     var req = new XMLHttpRequest();
-    req.open("POST", "http://localhost:5000/data");
+    req.open("POST", "/data");
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
             callback(req.responseText)
         } else {
-            console.error(req.status + " " + req.statusText + " " + url);
+            console.error(req.status + " " + req.statusText);
         }
     });
     req.addEventListener("error", function () {
-        console.error("Network error on " + url);
+        console.error("Network error");
     });
     req.send(entry);
 }
