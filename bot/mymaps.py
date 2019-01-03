5255934db6eb63329adc416579f469797782acc8
SebastienLesouef <sebastienlesouef@gmail.com>
2019-01-03 09:30:20 +0100

    create url added to mymaps. test green

--- bot/mymaps.py
+++ bot/mymaps.py
@@ -2,8 +2,17 @@
 # -*- coding:utf-8 -*-
 import requests
 
+from constants import URL, APIKEY
 
-def get_geocode():
-    url = 'test'
-    r = requests.get(url)
-    return r
+
+class Map:
+
+    def create_url(self, parsed_location):
+        parameters = "+".join(parsed_location)
+        call = "{}address={}&key={}".format(URL, parameters, APIKEY)
+        return call
+
+    def get_geocode(self):
+        url = 'test'
+        r = requests.get(url)
+        return r
--- bot/tests/test_mymaps.py
+++ bot/tests/test_mymaps.py
@@ -3,30 +3,41 @@ import bot.mymaps as script
 import requests
 
 
-def test_geocode_request(monkeypatch):
-    geocode_response = {
-       "results" : [
-          {
-             "formatted_address" : "7 Cité Paradis, 75010 Paris, France",
-             "geometry" : {
-                "location" : {
-                   "lat" : 48.8747578,
-                   "lng" : 2.350564700000001
-                },
-             },
-          }
-       ],
-       "status" : "OK"
-    }
 
-    def mockreturn(request):
-        return geocode_response
+class TestMaps:
 
-    monkeypatch.setattr(requests, 'get', mockreturn)
-    assert script.get_geocode() == geocode_response
+    @classmethod
+    def setup_class(cls):
+        cls.parameters = ["openclassrooms", "paris"]
+        cls.map = script.Map()
+        cls.url = ""
 
-# TODO: create test class
-# TODO: test call to gmaps (from parser content)
-# TODO: test response from gmaps stub
+    def test_create_url(self):
+        self.url = self.map.create_url(self.parameters)
+        for p in self.parameters:
+            assert p in self.url
+
+    def test_geocode_request(self, monkeypatch):
+        geocode_response = {
+           "results" : [
+              {
+                 "formatted_address" : "7 Cité Paradis, 75010 Paris, France",
+                 "geometry" : {
+                    "location" : {
+                       "lat" : 48.8747578,
+                       "lng" : 2.350564700000001
+                    },
+                 },
+              }
+           ],
+           "status" : "OK"
+        }
+
+        def mockreturn(request):
+            return geocode_response
+
+        monkeypatch.setattr(requests, 'get', mockreturn)
+        assert self.map.get_geocode() == geocode_response
+
 # TODO: test error cases (empty response, server error, ...)
 # TODO: test ajax response
