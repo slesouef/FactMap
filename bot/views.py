35815b7892cae16136fd89274888ef30b6c3504e
SebastienLesouef <sebastienlesouef@gmail.com>
2019-01-20 18:21:22 +0100

    pylint cleanup: docstrings added

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,9 @@
+"""main flask module
+
+responsible to launch Flask server
+responsible for the render of the bot web page
+responsible for the response to the ajax call
+"""
 from flask import Flask, render_template, request, jsonify
 from bot.myparser import Parser
 from bot.mymaps import Map
@@ -10,11 +16,14 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index/")
 def index():
+    """renders the webapp landing page"""
     return render_template("index.html")
 
 
 @app.route("/data", methods=["POST"])
 def parse():
+    """takes the data from POST and creates the response from backend
+    treatment"""
     myparser = Parser(DATA)
     mymap = Map()
     entry = request.data.decode()
