2cc1876e998a5a7afe5e8ed2a51342a4d75dc069
SebastienLesouef <sebastienlesouef@gmail.com>
2019-01-20 20:02:41 +0100

    pylint cleanup: docstrings added

--- bot/tests/test_views.py
+++ bot/tests/test_views.py
@@ -1,20 +1,26 @@
+"""Flask test module"""
 from bot.views import app
 
 
 class TestView:
+    """Test the Flask endpoints"""
 
     @classmethod
     def setup_class(cls):
+        """Setup Flask test client"""
         cls.client = app.test_client()
 
     def test_route_default(self):
+        """Test default route"""
         response = self.client.get("/")
         assert response.status_code == 200
 
     def test_route_index(self):
+        """Test /index route"""
         response = self.client.get("/index/")
         assert response.status_code == 200
 
     def test_route_time(self):
+        """Test AJAX request route"""
         response = self.client.post("/data")
         assert response.status_code == 200
