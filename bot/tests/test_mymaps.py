8f86557b5810034c22d05daf5bcd16f517d33c0b
SebastienLesouef <sebastienlesouef@gmail.com>
2019-01-22 14:24:27 +0100

    pylint cleanup: added docstrings fixed import order fixed constants names fixed constants format

--- bot/tests/test_mymaps.py
+++ bot/tests/test_mymaps.py
@@ -1,61 +1,69 @@
-import bot.mymaps as script
-
+"""Google API call test module"""
 import urllib.request
 import json
 
 from io import BytesIO
 
+import bot.mymaps as script
+
 from constants import URL
 
-geocode_response = {
-           "results" : [
-              {
-                 "formatted_address" : "7 Cité Paradis, 75010 Paris, France",
-                 "geometry" : {
-                    "location" : {
-                       "lat" : 48.8747578,
-                       "lng" : 2.350564700000001
-                    },
-                 },
-              }
-           ],
-           "status" : "OK"
+GEOCODE_RESPONSE = {
+    "results": [
+        {
+            "formatted_address": "7 Cité Paradis, 75010 Paris, France",
+            "geometry": {
+                "location": {
+                    "lat": 48.8747578,
+                    "lng": 2.350564700000001
+                },
+            },
         }
+    ],
+    "status": "OK"
+}
 
-empty_response = {
-   "results" : [],
-   "status" : "ZERO_RESULTS"
+EMPTY_RESPONSE = {
+    "results": [],
+    "status": "ZERO_RESULTS"
 }
 
 
 class TestMaps:
+    """Test the API call behaviour"""
 
     @classmethod
     def setup_class(cls):
+        """Setup test class with test parser result and load API caller"""
         cls.parameters = ["openclassrooms", "paris"]
         cls.map = script.Map()
 
     def test_create_url(self):
+        """Test the API URL construction from the parser results"""
         self.map.create_url(self.parameters)
-        for p in self.parameters:
-            assert p in self.map.url
+        for item in self.parameters:
+            assert item in self.map.url
 
     def test_get_geocode(self, monkeypatch):
+        """Mocked test of the call against API server"""
 
         def mockreturn(request):
-            return BytesIO(json.dumps(geocode_response).encode())
+            """Mock of response to the urllib.request method"""
+            return BytesIO(json.dumps(GEOCODE_RESPONSE).encode())
 
         monkeypatch.setattr(urllib.request, "urlopen", mockreturn)
-        assert self.map.get_geocode() == geocode_response
+        assert self.map.get_geocode() == GEOCODE_RESPONSE
 
     # server error ==> http 400+
     def test_get_geocode_error(self):
+        """Test of call method error handling using endpoint w/o parameters"""
         self.map.url = URL
         response = self.map.get_geocode()
         assert response == "INVALID REQUEST. ERROR CODE: 400"
 
     def test_extract_map_info(self):
-        self.map.extract_map_info(geocode_response)
+        """Test of API response treatment"""
+        self.map.extract_map_info(GEOCODE_RESPONSE)
         assert self.map.map_data["address"] == "7 Cité Paradis, 75010 Paris, " \
                                                "France"
         assert self.map.map_data["coordinates"] == (48.8747578,
@@ -63,5 +71,6 @@ class TestMaps:
 
     # empty response ==> http 200 status ZERO RESPONSE
     def test_extract_map_info_empty(self):
-        content = self.map.extract_map_info(empty_response)
+        """Test of empty result response from API"""
+        content = self.map.extract_map_info(EMPTY_RESPONSE)
         assert content == "INVALID REQUEST CONTENT"
