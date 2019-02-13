e5e2aff8bbb967e3ca8a186582a0cdc949a72f72
SebastienLesouef <sebastienlesouef@gmail.com>
2019-02-13 12:09:29 +0100

    favicon support added

--- bot/views.py
+++ bot/views.py
@@ -5,11 +5,12 @@ responsible to launch Flask server
 responsible for the render of the bot web page
 responsible for the response to the ajax call
 """
-from flask import Flask, render_template, request, jsonify
+import os
+
+from flask import Flask, render_template, request, jsonify, send_from_directory
 from bot.myparser import Parser
 from bot.mymaps import Map
 from bot.myknowledge import Extract
-
 
 
 APP = Flask(__name__)
@@ -25,19 +26,34 @@ def index():
     return render_template("index.html")
 
 
+@APP.route("/favicon.ico")
+def favicon():
+    """retuns the favicon from static/img"""
+    return send_from_directory(os.path.join(APP.root_path, "static"),
+                               "img/favicon.ico",
+                               mimetype="image/vnd.microsoft.icon")
+
+
+@APP.route("/browserconfig.xml")
+def browserconfig():
+    """returns browserconfig.xml from static folder"""
+    return send_from_directory(os.path.join(APP.root_path, "static"),
+                               "browserconfig.xml", mimetype="text/xml")
+
+
 @APP.route("/data", methods=["POST"])
 def response():
     """takes the data from POST and creates the response from backend
     treatment"""
-    response = {}
+    response_data = {}
     location = Parser().parse(request.data.decode())
     if not location:
         return jsonify({"error": "empty parse return"})
     my_map = Map().get_map_data(location)
-    response["map"] = my_map
-    if response["map"]["status"] == "OK":
+    response_data["map"] = my_map
+    if response_data["map"]["status"] == "OK":
         address = my_map["address"]
         target = Parser().parse(address)
         my_extract = Extract().get_text_extract(target)
-        response["wiki"] = my_extract
-    return jsonify(response)
+        response_data["wiki"] = my_extract
+    return jsonify(response_data)
